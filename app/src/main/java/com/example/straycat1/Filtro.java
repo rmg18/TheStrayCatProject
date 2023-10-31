6b9ffa8e74d4d0aa2dbf66b194a757e14be2ddbc
AidaFdz <aidafdz@hotmail.com>
2023-10-31 13:18:50 +0100

    Revert ".."
    This reverts commit 9e7a6a8658022615dbefdf0cd952a021f836da45.

--- app/src/main/java/com/example/straycat1/Filtro.java
+++ app/src/main/java/com/example/straycat1/Filtro.java
@@ -3,26 +3,12 @@ package com.example.straycat1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
 
 public class Filtro extends AppCompatActivity {
-
-    ImageView logotipo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtro);
-
-        logotipo = (ImageView) findViewById(R.id.avatar);
-
-        Glide.with(this)
-
-                .load("https://files.cults3d.com/uploaders/27695646/illustration-file/08001d33-2ba9-4b79-b635-f80138a1189b/ZBrush-Document_1.jpg")
-
-                .into(logotipo);
-
     }
 }
--- app/src/main/java/com/example/straycat1/MainActivity.java
+++ app/src/main/java/com/example/straycat1/MainActivity.java
@@ -2,9 +2,7 @@ package com.example.straycat1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void filtro(View view) {
-        Intent intentFiltro = new Intent(this, Filtro.class);
-
-        startActivity(intentFiltro);
-
     }
 }
